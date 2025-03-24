8097fb042c4847f3e0e3f918b7e77edf43f007e1
Michael Hackett <mhackett10@gmail.com>
2025-03-24 00:14:52 -0600

    Auto enter prose mode

--- daily.sh
+++ daily.sh
@@ -1,2 +1,2 @@
 DIR=$(dirname "$(realpath "$0")")
-vim $(python3 $DIR/daily.py)
+vim $(python3 $DIR/daily.py) -c "Prose" # Prose is a custom function I have that utilizes https://github.com/junegunn/goyo.vim?tab=readme-ov-file
